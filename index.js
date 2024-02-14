155054b595c902f82b0f078dbc87c581962bee22
RicoLittawa <ricolittawa030620@gmail.com>
2024-02-14 15:22:06 +0800

    install new dependencies

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ dotenv.config({ path: "./.env" });
 const app = express();
 const port = process.env.MYSQLPORT;
 app.use(express.json());
-// app.use(
-//   cors()
-// );
+app.use(
+  cors()
+);
 //Add reservation
 app.post("/", async (req, res) => {
   const data = { ...req.body.data };
@@ -29,43 +29,11 @@ app.get("/admin/scheduled-prayers", async (req, res) => {
 });
 //Get Count of Donation
 app.get("/admin", async (req, res) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://your-frontend.com"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
   const count = await GetTotalCountOfDonations();
   res.json(count);
 });
 
 app.get("/admin", async (req, res) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://your-frontend.com"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
   const count = await GetTotalCountOfRows();
   res.json(count);
 });
