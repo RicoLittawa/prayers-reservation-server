74e03784f3a963cb5474bdc106b3af2d0b31ef01
RicoLittawa <ricolittawa030620@gmail.com>
2024-02-14 14:26:48 +0800

    fix https to http

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://demo-prayers-reservation.netlify.app",
+    origin: "http://demo-prayers-reservation.netlify.app",
     methods: "GET,PUT,POST,DELETE",
     credentials: true,
   })
