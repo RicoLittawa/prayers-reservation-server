6db286da8382cab5b14b5fe5a5a312d8ed0cac6f
RicoLittawa <ricolittawa030620@gmail.com>
2024-02-14 19:24:06 +0800

    fix pool

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import mysql from "mysql2";
 
 dotenv.config({ path: "./.env" });
-export const Pool = mysql
+export const pool = mysql
   .createPool({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
@@ -11,19 +11,3 @@ export const Pool = mysql
   })
   .promise();
 
-  // Test the MySQL connection
-Pool.getConnection((err, connection) => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err.message);
-    return;
-  }
-  console.log('Connected to MySQL database');
-
-  // Release the connection
-  connection.release();
-});
-
-// Handle MySQL connection errors
-Pool.on('error', err => {
-  console.error('MySQL pool error:', err.message);
-});
--- database.js
+++ database.js
@@ -1,26 +1,26 @@
-import { Pool } from "./config.js";
+import { pool } from "./config.js";
 
 //Get
 export const GetList = async () => {
-  const [rows] = await Pool.query("select * from reservation_list");
+  const [rows] = await pool.query("select * from reservation_list");
   return rows;
 };
 
 export const GetTotalCountOfDonations = async () => {
-  const count = await Pool.query(
+  const count = await pool.query(
     "select sum(donation) as countOfDonations from reservation_list"
   );
   return count[0];
 };
 
 export const GetTotalCountOfRows = async () => {
-  const count = await Pool.query(
+  const count = await pool.query(
     "select count(*) as totalReservations from reservation_list"
   );
   return count[0];
 };
 export const GetListBaseOnReservationDate = async (reservationDate, time) => {
-  const [rows] = await Pool.query(
+  const [rows] = await pool.query(
     "select id,typeOfPrayer,prayersFor from reservation_list where reservationDate=? and timeOfMass=?",
     [reservationDate, time]
   );
@@ -37,7 +37,7 @@ export const AddReservation = async (data) => {
     timeOfMass,
     donation,
   ] = Object.values(data);
-  const result = await Pool.query(
+  const result = await pool.query(
     `insert into reservation_list (fullname,email,typeOfPrayer,prayersFor,reservationDate,timeOfMass,donation) values (?,?,?,?,?,?,?)`,
     [fullname, email, typeOfPrayer, prayersFor, date, timeOfMass, donation]
   );
@@ -45,7 +45,7 @@ export const AddReservation = async (data) => {
 };
 
 export const DeleteRow = async (id) => {
-  const result = await Pool.query("delete from reservation_list where id=?", [
+  const result = await pool.query("delete from reservation_list where id=?", [
     id,
   ]);
   return result;
